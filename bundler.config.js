3db96e22359e62bdc06afe8b5a2b3a71200cc06a
zhoupan <zhoupan@didiglobal.com>
2020-05-27 18:17:12 +0800

    first commit

---
+++ bundler.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './src',
+  output: './dist',
+  deploy: {
+    rootPath: 'xxx',
+    connectOptions: {
+      host: 'xxx',
+      user: 'xxx',
+      password: 'xxx'
+    }
+  }
+}
